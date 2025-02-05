96ffb95313d753d2591a87d58f144b6774055fd2
double-tilde <double-tilde@protonmail.com>
2025-02-05 13:58:20 +0000

    Adding cloak

--- config/nvim/lua/nv/plugins/modules.lua
+++ config/nvim/lua/nv/plugins/modules.lua
@@ -1,41 +1,23 @@
 return {
 	{
-		"echasnovski/mini.nvim",
-		name = "Mini Addons",
-		config = function()
-			-- Better Around/Inside textobjects
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [']quote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
-
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
-			require("mini.surround").setup()
-		end,
-	},
-	{
-		-- Undo Tree lets you go back through undo-history in a non-linear way
-		"mbbill/undotree",
-		name = "Undo Tree",
-		config = function()
-			vim.keymap.set("n", "<leader>-", vim.cmd.UndotreeToggle, { desc = "Toggle Undo Tree" })
-		end,
-	},
-	{
-		-- Detect tabstop and shiftwidth automatically
-		"tpope/vim-sleuth",
-		name = "Sleuth",
-	},
-	{
-		-- Highlight css / hex / rgb colors
-		"brenoprata10/nvim-highlight-colors",
-		name = "Highlight Colors",
-		config = function()
-			require("nvim-highlight-colors").setup({})
-		end,
+		"laytan/cloak.nvim",
+		name = "Cloak",
+		opts = {
+			patterns = {
+				{
+					file_pattern = ".env*",
+					cloak_pattern = { "=.+", ":.+", "-.+" },
+				},
+				{
+					file_pattern = "ENV",
+					cloak_pattern = { "=.+", ":.+", "-.+" },
+				},
+				{
+					file_pattern = "*.env",
+					cloak_pattern = { "=.+", ":.+", "-.+" },
+				},
+			},
+		},
 	},
 	{
 		-- Adds git related signs to the gutter, as well as utilities for managing changes
@@ -52,10 +34,50 @@ return {
 		},
 	},
 	{
+		-- Highlight RGB colors
+		"brenoprata10/nvim-highlight-colors",
+		name = "Highlight Colors",
+		opts = {},
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
+	},
+	{
+		"echasnovski/mini.nvim",
+		name = "Mini Addons",
+		opts = {},
+		config = function()
+			-- Better Around/Inside textobjects
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [']quote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({ n_lines = 500 })
+
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup()
+		end,
+	},
+	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		name = "Render Markdown",
+		opts = {},
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 		---@module 'render-markdown'
-		opts = {},
+	},
+	{
+		-- Undo Tree lets you go back through undo-history in a non-linear way
+		"mbbill/undotree",
+		name = "Undo Tree",
+		config = function()
+			vim.keymap.set("n", "<leader>-", vim.cmd.UndotreeToggle, { desc = "Toggle Undo Tree" })
+		end,
+	},
+	{
+		-- Detect tabstop and shiftwidth automatically
+		"tpope/vim-sleuth",
+		name = "Sleuth",
 	},
 }
