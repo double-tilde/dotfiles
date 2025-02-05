c955683786350e6d58c990ba65a199e283bafdbd
double-tilde <double-tilde@protonmail.com>
2025-02-05 20:39:37 +0000

    New screenshot

--- config/nvim/lua/nv/plugins/theme.lua
+++ config/nvim/lua/nv/plugins/theme.lua
@@ -1,22 +1,22 @@
 return {
-		"dgox16/oldworld.nvim",
-		name = "Old World Theme",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("oldworld").setup({
-				transparent = true,
-			})
+	"dgox16/oldworld.nvim",
+	name = "Old World Theme",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("oldworld").setup({
+			transparent = true,
+		})
 
-			vim.cmd.colorscheme("oldworld")
+		vim.cmd.colorscheme("oldworld")
 
-			-- Background Opacity
-			vim.cmd.hi("Normal guibg=none")
-			vim.cmd.hi("NonText guibg=none")
-			vim.cmd.hi("Normal ctermbg=none")
-			vim.cmd.hi("NonText ctermbg=none")
+		-- Background Opacity
+		vim.cmd.hi("Normal guibg=none")
+		vim.cmd.hi("NonText guibg=none")
+		vim.cmd.hi("Normal ctermbg=none")
+		vim.cmd.hi("NonText ctermbg=none")
 
-			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
-		end,
+		-- You can configure highlights by doing something like:
+		vim.cmd.hi("Comment gui=none")
+	end,
 }
