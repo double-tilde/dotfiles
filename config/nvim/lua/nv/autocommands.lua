1caff92021d2f66b4e7666d8cab733d5e30f3ea8
double-tilde <double-tilde@protonmail.com>
2025-02-12 15:25:13 +0000

    Showing command line in neovim

--- config/nvim/lua/nv/autocommands.lua
+++ config/nvim/lua/nv/autocommands.lua
@@ -18,3 +18,17 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 		vim.bo.filetype = "html"
 	end,
 })
+
+-- Show the commandline when I am recording a macro, then hide it again when I am not
+-- This setting is needed as in my base.lua file I have hidden the command line by
+-- default for aesthetic reasons
+vim.api.nvim_create_autocmd("RecordingEnter", {
+	callback = function()
+		vim.opt.cmdheight = 1
+	end,
+})
+vim.api.nvim_create_autocmd("RecordingLeave", {
+	callback = function()
+		vim.opt.cmdheight = 0
+	end,
+})
