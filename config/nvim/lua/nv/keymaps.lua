1077d8ce82fbc40f025b0e9644d8fc6ecc673255
double-tilde <double-tilde@protonmail.com>
2025-02-25 14:41:47 +0000

    Changing alt to ctrl

--- config/nvim/lua/nv/keymaps.lua
+++ config/nvim/lua/nv/keymaps.lua
@@ -73,13 +73,13 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 
 -- Remapping common syntax
-vim.keymap.set("i", "<C-u>", "=", { desc = "Press CTRL+u to insert an equals symbol" })
-vim.keymap.set("i", "<C-i>", "+", { desc = "Press CTRL+i to insert a plus symbol" })
-vim.keymap.set("i", "<C-o>", "-", { desc = "Press CTRL+o to insert a minus symbol" })
-vim.keymap.set("i", "<C-p>", "{}<Esc>i", { desc = "Press CTRL+p to insert curly brackets and put cursor inside" })
-vim.keymap.set("i", "<C-n>", '""<Esc>i', { desc = "Press CTRL+n to insert double quotes and put cursor inside" })
-vim.keymap.set("i", "<C-d>", "()<Esc>i", { desc = "Press CTRL+d to insert brackets and put cursor inside" })
-vim.keymap.set("i", "<C-f>", "[]<Esc>i", { desc = "Press CTRL+f to insert square brackets and put cursor inside" })
+vim.keymap.set("i", "<M-u>", "=", { desc = "Press ALT+u to insert an equals symbol" })
+vim.keymap.set("i", "<M-i>", "+", { desc = "Press ALT+i to insert a plus symbol" })
+vim.keymap.set("i", "<M-o>", "-", { desc = "Press ALT+o to insert a minus symbol" })
+vim.keymap.set("i", "<M-p>", "{}<Esc>i", { desc = "Press ALT+p to insert curly brackets and put cursor inside" })
+vim.keymap.set("i", "<M-j>", '""<Esc>i', { desc = "Press ALT+j to insert double quotes and put cursor inside" })
+vim.keymap.set("i", "<M-k>", "()<Esc>i", { desc = "Press ALT+k to insert brackets and put cursor inside" })
+vim.keymap.set("i", "<M-l>", "[]<Esc>i", { desc = "Press ALT+l to insert square brackets and put cursor inside" })
 
 -- Quickfix list navigation - using pcall to stop E553 No more items
 vim.keymap.set("n", "<C-n>", function()
