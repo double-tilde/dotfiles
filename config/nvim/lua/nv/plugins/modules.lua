018b1939e04d297e495eb2a96d7b69660cb602a9
double-tilde <double-tilde@protonmail.com>
2025-03-02 15:40:23 +0000

    Fixing noice issues

--- config/nvim/lua/nv/plugins/modules.lua
+++ config/nvim/lua/nv/plugins/modules.lua
@@ -64,13 +64,26 @@ return {
 		config = function()
 			require("noice").setup({
 				vim.cmd([[highlight NoiceCmdlinePopup guibg=#161617]]),
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				presets = {
+					bottom_search = true,
+					command_palette = true,
+					long_message_to_split = true,
+					lsp_doc_border = true,
+				},
 			})
 			require("notify").setup({
 				stages = "static",
 				render = "minimal",
-				background_colour = "#161617",
 				timeout = 2000,
-				-- FIX: When using visual line switch, stop cmdline popup
+				background_colour = "#161617",
 			})
 		end,
 	},
