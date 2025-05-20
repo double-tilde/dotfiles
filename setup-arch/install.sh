33ea73cb4a2c333daa3f3921278d8bd0578e7214
Double Tilde <double-tilde@protonmail.com>
2025-05-20 17:20:22 +0100

    Adding swaync

--- setup-arch/install.sh
+++ setup-arch/install.sh
@@ -36,8 +36,8 @@ fi
 
 # Install hyprland
 if ! command -v hyprland &>/dev/null; then
-	yay -S --noconfirm wayland hyprland xdg-desktop-portal xdg-desktop-portal-wlr dunst waybar \
-	hyprpaper grim slurp wl-clipboard wofi dunst swaylock-effects swayidle || { echo "failed to install hyprland"; return 1; }
+	yay -S --noconfirm wayland hyprland xdg-desktop-portal xdg-desktop-portal-wlr waybar \
+	hyprpaper grim slurp wl-clipboard wofi swaync swaylock-effects swayidle || { echo "failed to install hyprland"; return 1; }
 fi
 
 # Install fonts
@@ -73,7 +73,7 @@ fi
 
 # Install extras
 if ! command -v firefox &>/dev/null; then
-	yay -S --noconfirm firefox dolphin obs-studio obsidian vlc mpv gimp openvpn \
+	yay -S --noconfirm firefox thunar obs-studio obsidian vlc mpv gimp openvpn \
 	htop fastfetch brightnessctl || { echo "failed to install extra tools"; return 1; }
 fi
 
