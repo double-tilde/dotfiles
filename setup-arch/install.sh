cd2fe14dbf3f798e3df86542b6c2de02c9d013a8
Double Tilde <double-tilde@protonmail.com>
2025-05-24 14:45:38 +0100

    Tidying install script

--- setup-arch/install.sh
+++ setup-arch/install.sh
@@ -13,33 +13,39 @@ for dir in alacritty bash dunst fastfetch kitty nvim tmux waybar wofi zsh setup-
 done
 cd ~
 
-# Install programming languges
-if ! command -v npm &>/dev/null; then
-	cd ~ && mkdir ~/.local && mkdir ~/.local/share && mkdir ~/.local/share/nvm && \
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash \
-	&& nvm install --lts || { echo "failed to install npm"; return 1; }
-fi
-
-curl https://sh.rustup.rs -sSf | sh || { echo "failed to install rust"; return 1; }
-sudo mv ~/.cargo ~/.config/cargo && sudo mv ~/.rustup ~/.config/rustup
-
-
-if ! command -v go &>/dev/null; then
-	yay -S --noconfirm go python python-pip || { echo "failed to install go any python"; return 1; }
-fi
-
 # Install yay
 if ! command -v yay &>/dev/null; then
 	git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si \
 	&& cd .. && rm -rf yay && yay -Syu || { echo "failed to install yay"; return 1; }
 fi
 
+# Install programming languages
+# if ! command -v npm &>/dev/null; then
+# 	cd ~ && mkdir ~/.local && mkdir ~/.local/share && mkdir ~/.local/share/nvm && \
+# 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash \
+# 	&& nvm install --lts || { echo "failed to install npm"; return 1; }
+# fi
+cd ~ && mkdir ~/.local && mkdir ~/.local/share && mkdir ~/.local/share/nvm && \
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash \
+&& nvm install --lts || { echo "failed to install npm"; return 1; }
+
+curl https://sh.rustup.rs -sSf | sh || { echo "failed to install rust"; return 1; }
+sudo mv ~/.cargo ~/.config/cargo && sudo mv ~/.rustup ~/.config/rustup
+
+# if ! command -v go &>/dev/null; then
+# 	yay -S --noconfirm go python python-pip || { echo "failed to install go any python"; return 1; }
+# fi
+yay -S --noconfirm go python python-pip || { echo "failed to install go any python"; return 1; }
+
 # Install hyprland
-if ! command -v hyprland &>/dev/null; then
-	yay -S --noconfirm wayland hyprland xdg-desktop-portal xdg-desktop-portal-wlr waybar \
-	grim slurp jq hyprpicker hyprshot hyprpaper wl-clipboard wofi swaync swaylock-effects swayidle \
-	|| { echo "failed to install hyprland"; return 1; }
-fi
+# if ! command -v hyprland &>/dev/null; then
+# 	yay -S --noconfirm wayland hyprland xdg-desktop-portal xdg-desktop-portal-wlr waybar \
+# 	grim slurp jq hyprpicker hyprshot hyprpaper wl-clipboard wofi swaync swaylock-effects swayidle \
+# 	|| { echo "failed to install hyprland"; return 1; }
+# fi
+yay -S --noconfirm wayland hyprland xdg-desktop-portal xdg-desktop-portal-wlr waybar \
+grim slurp jq hyprpicker hyprshot hyprpaper wl-clipboard wofi swaync swaylock-effects swayidle \
+|| { echo "failed to install hyprland"; return 1; }
 
 # Install fonts
 yay -S --noconfirm ttf-karla otf-geist-mono-nerd ttf-hack-nerd ttf-firacode-nerd \
@@ -48,10 +54,12 @@ adobe-source-sans-fonts adobe-source-serif-fonts noto-fonts noto-fonts-cjk \
 noto-fonts-emoji noto-fonts-extra font-manager || { echo "failed to install fonts"; return 1; }
 
 # Install development tools
-if ! command -v zsh &>/dev/null; then
-	yay -S --noconfirm alacitty kitty zsh tmux fzf ripgrep neovim bat tldr openssh \
-	||  { echo "failed to install development tools"; return 1; }
-fi
+# if ! command -v zsh &>/dev/null; then
+# 	yay -S --noconfirm alacitty kitty zsh tmux fzf ripgrep neovim bat tldr openssh \
+# 	||  { echo "failed to install development tools"; return 1; }
+# fi
+yay -S --noconfirm alacitty kitty zsh tmux fzf ripgrep neovim bat tldr openssh \
+||  { echo "failed to install development tools"; return 1; }
 
 # Install Oh My Zsh
 if command -v zsh; then
@@ -67,16 +75,20 @@ if command -v tmux; then
 fi
 
 # Install Audio tools
-if ! command -v pavucontrol &>/dev/null; then
-	yay -S --noconfirm pipewire pipewire-pulse wireplumber pavucontrol \
-	||  { echo "failed to install audio tools"; return 1; }
-fi
+# if ! command -v pavucontrol &>/dev/null; then
+# 	yay -S --noconfirm pipewire pipewire-pulse wireplumber pavucontrol \
+# 	||  { echo "failed to install audio tools"; return 1; }
+# fi
+yay -S --noconfirm pipewire pipewire-pulse wireplumber pavucontrol \
+||  { echo "failed to install audio tools"; return 1; }
 
 # Install extras
-if ! command -v firefox &>/dev/null; then
-	yay -S --noconfirm firefox thunar obs-studio obsidian vlc imv mpv gimp openvpn \
-	htop fastfetch brightnessctl || { echo "failed to install extra tools"; return 1; }
-fi
+# if ! command -v firefox &>/dev/null; then
+# 	yay -S --noconfirm firefox thunar obs-studio obsidian vlc imv mpv gimp openvpn \
+# 	htop fastfetch brightnessctl || { echo "failed to install extra tools"; return 1; }
+# fi
+yay -S --noconfirm firefox thunar obs-studio obsidian vlc imv mpv gimp openvpn \
+htop fastfetch brightnessctl || { echo "failed to install extra tools"; return 1; }
 
 # Enable system services
 sudo systemctl enable --now NetworkManager
