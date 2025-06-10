a0565ca6537980215ee885d8c24c6b5e85ab8323
Double Tilde <double-tilde@protonmail.com>
2025-06-10 16:27:00 +0100

    Making tmux more minimal

--- nvim/.config/nvim/lua/nv/autocommands.lua
+++ nvim/.config/nvim/lua/nv/autocommands.lua
@@ -22,13 +22,13 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 -- Show the commandline when I am recording a macro, then hide it again when I am not
 -- This setting is needed as in my base.lua file I have hidden the command line by
 -- default for aesthetic reasons
--- vim.api.nvim_create_autocmd("RecordingEnter", {
--- 	callback = function()
--- 		vim.opt.cmdheight = 1
--- 	end,
--- })
--- vim.api.nvim_create_autocmd("RecordingLeave", {
--- 	callback = function()
--- 		vim.opt.cmdheight = 0
--- 	end,
--- })
+vim.api.nvim_create_autocmd("RecordingEnter", {
+	callback = function()
+		vim.opt.cmdheight = 1
+	end,
+})
+vim.api.nvim_create_autocmd("RecordingLeave", {
+	callback = function()
+		vim.opt.cmdheight = 0
+	end,
+})
