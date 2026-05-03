a4967ea5eb15e004a41747748ecd1a168a28eace
Double Tilde <double-tilde@protonmail.com>
2026-05-03 17:20:36 +0100

    removing old hyprland tmux command

--- arch-setup/.local/scripts/tmux-startup.sh
+++ arch-setup/.local/scripts/tmux-startup.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-# Start new session (or attach if exists)
-tmux new-session -d -s main
+# Start new session
+tmux new-session -d -s dt
 
-# Split: right pane (60%)
-tmux split-window -h -p 60
+# Right pane
+tmux split-window -h -p 65
 
-# Go back to left pane
+# Go to left pane
 tmux select-pane -L
 
-# Split left vertically (top/bottom)
+# Split left vertically
 tmux split-window -v
 
-# Top-left: tty-clock
+# tty-clock
 tmux select-pane -U
 tmux send-keys "tty-clock" C-m
 
-# Bottom-left: htop
+# htop
 tmux select-pane -D
 tmux send-keys "htop" C-m
 
 tmux select-pane -R
 
-tmux attach-session -t main
+tmux attach-session -t dt
