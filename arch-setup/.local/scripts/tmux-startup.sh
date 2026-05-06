a88ca39a7834f24e62de8febaab0915503349cab
Double Tilde <double-tilde@protonmail.com>
2026-05-06 20:04:34 +0100

    making better tmux split on laptop

--- arch-setup/.local/scripts/tmux-startup.sh
+++ arch-setup/.local/scripts/tmux-startup.sh
@@ -8,14 +8,15 @@ COLS=$2
 # New session with dimension (subtract 1 row for status bar)
 tmux new-session -d -s dt -x "$COLS" -y "$((ROWS - 1))"
 
-# Right pane
-tmux split-window -h -p 75
-
-# Go to left pane
-tmux select-pane -L
 
 # If the screen is wide enough
 if [[ $COLS -ge 150 ]]; then
+	# Right pane
+	tmux split-window -h -p 75
+	
+	# Go to left pane
+	tmux select-pane -L
+
 	# Split left vertically
 	tmux split-window -v
 	
@@ -27,6 +28,12 @@ if [[ $COLS -ge 150 ]]; then
 	tmux select-pane -D
 	tmux send-keys "glances --disable-left-sidebar" C-m
 else
+	# Right pane
+	tmux split-window -h -p 70
+	
+	# Go to left pane
+	tmux select-pane -L
+
 	# tty-clock
 	tmux send-keys "tty-clock -c" C-m
 fi
